911bddbc0644b45f3dceaae431418ce810d5dda5
Arun Patra <arunkpatra@gmail.com>
2020-10-23 13:23:56 +0530

    Initial commit

---
+++ src/main/java/com/bhn/blueberry/BlueBerryApplication.java
@@ -0,0 +1,13 @@
+package com.bhn.blueberry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueBerryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueBerryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bhn/blueberry/BlueBerryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhn.blueberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueBerryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
